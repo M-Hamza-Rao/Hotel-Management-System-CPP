d840615cbfd3897367d22aa5822df09d1ba59988
M-Hamza-Rao <rmuhammadhamza86@gmail.com>
2025-05-06 05:05:33 +0200

    Header

--- Reservation.h
+++ Reservation.h
@@ -16,9 +16,15 @@ private:
 public:
     Reservation(int res_id, std::string cus_id, int room_id, std::string check_in, std::string check_out, std::string payment_status, double amount);
     void makeReservation(MYSQL *conn);
+    static void viewReservationsByCustomer(MYSQL *conn, std::string customer_id);
     static void removeReservation(MYSQL *conn, int room_id);
     static void updatePaymentStatus(MYSQL *conn, std::string customer_id, std::string status);
     void displayReservationInfo();
+    static void getReservationByID(MYSQL *conn, int reservation_id);
+    static void viewAllActiveReservations(MYSQL *conn);
+    static void viewAllCancelledReservations(MYSQL *conn);
+
+
 };
 
 #endif // RESERVATION_H

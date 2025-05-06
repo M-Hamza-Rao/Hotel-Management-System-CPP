853825704b727d2cb0b3cb26facc3afbf7f51e2c
M-Hamza-Rao <rmuhammadhamza86@gmail.com>
2025-05-06 05:02:36 +0200

    Customer

--- Customer.cpp
+++ Customer.cpp
@@ -21,3 +21,54 @@ void Customer::addCustomer(MYSQL *conn) {
     }
 }
 
+string Customer::getCustomerNameByID(MYSQL *conn, string customer_id) {
+    string query = "SELECT name FROM Customers WHERE customer_id = '" + customer_id + "'";
+
+    if (mysql_query(conn, query.c_str())) {
+        cerr << "Error fetching customer name: " << mysql_error(conn) << endl;
+        return "";
+    }
+
+    MYSQL_RES *res = mysql_store_result(conn);
+    MYSQL_ROW row = mysql_fetch_row(res);
+
+    string name = "";
+    if (row) {
+        name = row[0];
+    }
+
+    mysql_free_result(res);
+    return name;
+}
+
+void Customer::showCustomerInfoByID(MYSQL *conn) {
+	string customer_id;
+    cout << "Enter customer ID: ";
+    cin >> customer_id;
+    string query = "SELECT customer_id, name, address, phone, email FROM Customers WHERE customer_id = '" + customer_id + "'";
+
+    if (mysql_query(conn, query.c_str())) {
+        cerr << "Error fetching customer details: " << mysql_error(conn) << endl;
+        return;
+    }
+
+    MYSQL_RES *res = mysql_store_result(conn);
+    MYSQL_ROW row = mysql_fetch_row(res);
+
+    if (!row) {
+        cout << "No customer found with ID: " << customer_id << endl;
+    } else {
+        // Print customer details
+        cout << "\nCustomer Details:" << endl;
+        cout << "Customer ID: " << row[0] << endl;
+        cout << "Name: " << row[1] << endl;
+        cout << "Address: " << row[2] << endl;
+        cout << "Phone: " << row[3] << endl;
+        cout << "Email: " << row[4] << endl;
+    }
+
+    mysql_free_result(res);
+}
+
+
+
